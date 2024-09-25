50e09c61c520b19a86cc8fcc8ea7b9ddd9264b10
CariCard <112133987+CariCard@users.noreply.github.com>
2024-09-25 15:05:59 +0000

    added questions from quiz i got wrong

--- testingCode.c
+++ testingCode.c
@@ -1,9 +1,19 @@
 
 
 int main(){
-int counter;
+/*int counter;
 
 for(int x = 0; x<=6; x+=2){
   counter +=x;
 }
+*/
+
+int arr[5] = {1, 2, 3, 4, 5};
+int *ptr = arr;
+int sum = 0;
+  for(int i =0; i<5; ++i){
+    sum+= *(ptr +i);
+  }
+printf("sum of array elements: %d\n", &sum);
+return 0;
 }
